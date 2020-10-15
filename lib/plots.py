a74a015588958e691207b9a0e61925450af93683
eximus <fabio_r11@hotmail.com>
2020-10-15 19:20:04 +0100

    fixe leftover problems

--- lib/plots.py
+++ lib/plots.py
@@ -1,23 +1,32 @@
+import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib import ticker
+from pylab import DateFormatter
 
-matplotlib.rcParams.update({'font.size': sl}); plt.rc('font', size=sl)
-matplotlib.rc('xtick', labelsize=sl); matplotlib.rc('ytick', labelsize=sl); matplotlib.rcParams.update({'font.size': sl})
+SL = 13
 
-def chicklet_plot(img_name, data, lev, cmap=plt.cm.jet, extend="both"):
-    fig, ax1 = plt.subplots(1,figsize=(16,4), sharex=True, sharey=True)
-    plt.xlabel("Time (month/year)",size=sl)
-    im1 = ax1.contourf(datm,(eft/3600.)/24.,data,lev,cmap=cmap,extend=extend)
-    divider = make_axes_locatable(ax1)
-    cax = divider.append_axes("right", size="2%", pad=0.2)
-    cb = plt.colorbar(im1, cax=cax)
-    tick_locator = ticker.MaxNLocator(nbins=7)
-    cb.locator = tick_locator
-    cb.update_ticks()
-    ax1.xaxis.set_major_locator(mdates.MonthLocator(interval=2))
-    ax1.xaxis.set_major_formatter( DateFormatter('%m/%Y') )
-    plt.tight_layout()
-    plt.axis('tight')
-    fig.text(0.000, 0.53, 'Forecast Time (Days)', va='center', rotation='vertical',size=sl)
-    plt.savefig(img_name, dpi=300, facecolor='w', edgecolor='w',orientation='portrait', papertype=None, format='png',transparent=False, bbox_inches='tight', pad_inches=0.1)
-    plt.close(fig)
+matplotlib.rcParams.update({'font.size': SL})
+plt.rc('font', size=SL)
+matplotlib.rc('xtick', labelsize=SL)
+matplotlib.rc('ytick', labelsize=SL)
+matplotlib.rcParams.update({'font.size': SL})
+
+def chicklet_plot(img_name, data, date_time, forecast_time, lev, cmap=plt.cm.jet, extend="both"):
+	fig, ax1 = plt.subplots(1,figsize=(16,4), sharex=True, sharey=True)
+	plt.xlabel("Time (month/year)",size=SL)
+	im1 = ax1.contourf(date_time,(forecast_time/3600.)/24.,data,lev,cmap=cmap,extend=extend)
+	divider = make_axes_locatable(ax1)
+	cax = divider.append_axes("right", size="2%", pad=0.2)
+	cb = plt.colorbar(im1, cax=cax)
+	tick_locator = ticker.MaxNLocator(nbins=7)
+	cb.locator = tick_locator
+	cb.update_ticks()
+	ax1.xaxis.set_major_locator(mdates.MonthLocator(interval=2))
+	ax1.xaxis.set_major_formatter( DateFormatter('%m/%Y') )
+	plt.tight_layout()
+	plt.axis('tight')
+	fig.text(0.000, 0.53, 'Forecast Time (Days)', va='center', rotation='vertical',size=SL)
+	plt.savefig(img_name, dpi=300, facecolor='w', edgecolor='w',orientation='portrait', papertype=None, format='png',transparent=False, bbox_inches='tight', pad_inches=0.1)
+	plt.close(fig)
--- testChicletPlot.py
+++ testChicletPlot.py
@@ -1,3 +1,5 @@
+#!/home/rmc/progs/python/anaconda3/bin/ipython --pylab=auto
+
 import os
 import netCDF4 as nc
 import numpy as np
@@ -43,27 +45,27 @@ lev= np.linspace(1001.,np.nanmax(np.append(omsl,np.nanmean(emsl,axis=1))),100)
 lev2= np.linspace(0.,1.9,100)
 lev3= np.linspace(-4.6,4.6,101)
 for i in ssp:
-    auxo=np.copy(omsl[i,:,:].T)
-    auxm=np.copy(np.nanmean(emsl[i,:,:,:],axis=0).T)
-    auxm2=np.copy(emsl[i,0,:,:].T)
-    auxmsprd=np.copy(np.std(emsl[i,:,:,:],axis=0).T)
-    # Observation
-    img_name = 'ChicletChart_Observation_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
-    chicklet_plot(img_name, auxo, lev)
-    # Ensemble Mean
-    img_name = 'ChicletChart_ModelEM_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
-    chicklet_plot(img_name, auxm, lev)
-    # First Ensemble Member
-    img_name = 'ChicletChart_Modelm1_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
-    chicklet_plot(img_name, auxm2, lev)
-    # Spread
-    img_name = 'ChicletChart_ModelSpread_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
-    chicklet_plot(img_name, auxmsprd, lev2, extend='max')
-    # Differences
-    img_name = 'ChicletChart_DiffModelEMObs_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
-    chicklet_plot(img_name, auxm - auxo, lev3, cmap=plt.cm.RdBu_r)
-    img_name = 'ChicletChart_DiffModelm1Obs_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
-    chicklet_plot(img_name, auxm2 - auxo, lev3, cmap=plt.cm.RdBu_r)
+	auxo=np.copy(omsl[i,:,:].T)
+	auxm=np.copy(np.nanmean(emsl[i,:,:,:],axis=0).T)
+	auxm2=np.copy(emsl[i,0,:,:].T)
+	auxmsprd=np.copy(np.std(emsl[i,:,:,:],axis=0).T)
+	# Observation
+	img_name = 'ChicletChart_Observation_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
+	chicklet_plot(img_name, auxo, datm, eft, lev)
+	# Ensemble Mean
+	img_name = 'ChicletChart_ModelEM_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
+	chicklet_plot(img_name, auxm, datm, eft, lev)
+	# First Ensemble Member
+	img_name = 'ChicletChart_Modelm1_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
+	chicklet_plot(img_name, auxm2, datm, eft, lev)
+	# Spread
+	img_name = 'ChicletChart_ModelSpread_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
+	chicklet_plot(img_name, auxmsprd, datm, eft, lev2, extend='max')
+	# Differences
+	img_name = 'ChicletChart_DiffModelEMObs_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
+	chicklet_plot(img_name, auxm - auxo, datm, eft, lev3, cmap=plt.cm.RdBu_r)
+	img_name = 'ChicletChart_DiffModelm1Obs_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
+	chicklet_plot(img_name, auxm2 - auxo, datm, eft, lev3, cmap=plt.cm.RdBu_r)
 
 # Temperature
 Nvar = Nvaren[1]
@@ -71,27 +73,27 @@ lev= np.linspace(23,31,100)
 lev2= np.linspace(0.,1.3,100)
 lev3= np.linspace(-3.1,3.1,101)
 for i in ssp:
-    auxo=np.copy(oatmp[i,:,:].T)
+	auxo=np.copy(oatmp[i,:,:].T)
 	auxm=np.copy(np.nanmean(eatmp[i,:,:,:],axis=0).T)
 	auxm2=np.copy(eatmp[i,0,:,:].T)
 	auxmsprd=np.copy(np.std(eatmp[i,:,:,:],axis=0).T)
-    # Observation
-    img_name = 'ChicletChart_Observation_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
-    chicklet_plot(img_name, auxo, lev)
-    # Ensemble Mean
-    img_name = 'ChicletChart_ModelEM_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
-    chicklet_plot(img_name, auxm, lev)
-    # First Ensemble Member
-    img_name = 'ChicletChart_Modelm1_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
-    chicklet_plot(img_name, auxm2, lev)
-    # Spread
-    img_name = 'ChicletChart_ModelSpread_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
-    chicklet_plot(img_name, auxmsprd, lev2, extend='max')
-    # Differences
-    img_name = 'ChicletChart_DiffModelEMObs_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
-    chicklet_plot(img_name, auxm - auxo, lev3, cmap=plt.cm.RdBu_r)
-    img_name = 'ChicletChart_DiffModelm1Obs_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
-    chicklet_plot(img_name, auxm2 - auxo, lev3, cmap=plt.cm.RdBu_r)
+	# Observation
+	img_name = 'ChicletChart_Observation_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
+	chicklet_plot(img_name, auxo, datm, eft, lev)
+	# Ensemble Mean
+	img_name = 'ChicletChart_ModelEM_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
+	chicklet_plot(img_name, auxm, datm, eft, lev)
+	# First Ensemble Member
+	img_name = 'ChicletChart_Modelm1_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
+	chicklet_plot(img_name, auxm2, datm, eft, lev)
+	# Spread
+	img_name = 'ChicletChart_ModelSpread_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
+	chicklet_plot(img_name, auxmsprd, datm, eft, lev2, extend='max')
+	# Differences
+	img_name = 'ChicletChart_DiffModelEMObs_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
+	chicklet_plot(img_name, auxm - auxo, datm, eft, lev3, cmap=plt.cm.RdBu_r)
+	img_name = 'ChicletChart_DiffModelm1Obs_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
+	chicklet_plot(img_name, auxm2 - auxo, datm, eft, lev3, cmap=plt.cm.RdBu_r)
 
 # Humidade
 Nvar = Nvaren[2]
@@ -99,27 +101,27 @@ lev= np.linspace(65,100,100)
 lev2= np.linspace(0.,8.,100)
 lev3= np.linspace(-15.1,15.1,101)
 for i in ssp:
-    auxo=np.copy(orh[i,:,:].T)
+	auxo=np.copy(orh[i,:,:].T)
 	auxm=np.copy(np.nanmean(erh[i,:,:,:],axis=0).T)
 	auxm2=np.copy(erh[i,0,:,:].T)
 	auxmsprd=np.copy(np.std(erh[i,:,:,:],axis=0).T)
-    # Observation
-    img_name = 'ChicletChart_Observation_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
-    chicklet_plot(img_name, auxo, lev, extend='min')
-    # Ensemble Mean
-    img_name = 'ChicletChart_ModelEM_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
-    chicklet_plot(img_name, auxm, lev, extend='min')
-    # First Ensemble Member
-    img_name = 'ChicletChart_Modelm1_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
-    chicklet_plot(img_name, auxm2, lev, extend='min')
-    # Spread
-    img_name = 'ChicletChart_ModelSpread_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
-    chicklet_plot(img_name, auxmsprd, lev2, extend='max')
-    # Differences
-    img_name = 'ChicletChart_DiffModelEMObs_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
-    chicklet_plot(img_name, auxm - auxo, lev3, cmap=plt.cm.RdBu_r)
-    img_name = 'ChicletChart_DiffModelm1Obs_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
-    chicklet_plot(img_name, auxm2 - auxo, lev3, cmap=plt.cm.RdBu_r)
+	# Observation
+	img_name = 'ChicletChart_Observation_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
+	chicklet_plot(img_name, auxo, datm, eft, lev, extend='min')
+	# Ensemble Mean
+	img_name = 'ChicletChart_ModelEM_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
+	chicklet_plot(img_name, auxm, datm, eft, lev, extend='min')
+	# First Ensemble Member
+	img_name = 'ChicletChart_Modelm1_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
+	chicklet_plot(img_name, auxm2, datm, eft, lev, extend='min')
+	# Spread
+	img_name = 'ChicletChart_ModelSpread_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
+	chicklet_plot(img_name, auxmsprd, datm, eft, lev2, extend='max')
+	# Differences
+	img_name = 'ChicletChart_DiffModelEMObs_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
+	chicklet_plot(img_name, auxm - auxo, datm, eft, lev3, cmap=plt.cm.RdBu_r)
+	img_name = 'ChicletChart_DiffModelm1Obs_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
+	chicklet_plot(img_name, auxm2 - auxo, datm, eft, lev3, cmap=plt.cm.RdBu_r)
 
 # Wind Speed
 Nvar = Nvaren[3]+NvarAnh[0]
@@ -127,25 +129,25 @@ lev= np.linspace(0,13.,100)
 lev2= np.linspace(0.,2.8,100)
 lev3= np.linspace(-4.6,4.6,101)
 for i in ssp:
-    auxo=np.copy(owspa[i,:,:].T)
+	auxo=np.copy(owspa[i,:,:].T)
 	auxm=np.copy(np.nanmean(ewspa[i,:,:,:],axis=0).T)
 	auxm2=np.copy(ewspa[i,0,:,:].T)
 	auxmsprd=np.copy(np.std(ewspa[i,:,:,:],axis=0).T)
-    # Observation
-    img_name = 'ChicletChart_Observation_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
-    chicklet_plot(img_name, auxo, lev, extend='max')
-    # Ensemble Mean
-    img_name = 'ChicletChart_ModelEM_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
-    chicklet_plot(img_name, auxm, lev, extend='max')
-    # First Ensemble Member
-    img_name = 'ChicletChart_Modelm1_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
-    chicklet_plot(img_name, auxm2, lev, extend='max')
-    # Spread
-    img_name = 'ChicletChart_ModelSpread_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
-    chicklet_plot(img_name, auxmsprd, lev2, extend='max')
-    # Differences
-    img_name = 'ChicletChart_DiffModelEMObs_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
-    chicklet_plot(img_name, auxm - auxo, lev3, cmap=plt.cm.RdBu_r)
-    img_name = 'ChicletChart_DiffModelm1Obs_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
-    chicklet_plot(img_name, auxm2 - auxo, lev3, cmap=plt.cm.RdBu_r)
+	# Observation
+	img_name = 'ChicletChart_Observation_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
+	chicklet_plot(img_name, auxo, datm, eft, lev, extend='max')
+	# Ensemble Mean
+	img_name = 'ChicletChart_ModelEM_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
+	chicklet_plot(img_name, auxm, datm, eft, lev, extend='max')
+	# First Ensemble Member
+	img_name = 'ChicletChart_Modelm1_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
+	chicklet_plot(img_name, auxm2, datm, eft, lev, extend='max')
+	# Spread
+	img_name = 'ChicletChart_ModelSpread_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
+	chicklet_plot(img_name, auxmsprd, datm, eft, lev2, extend='max')
+	# Differences
+	img_name = 'ChicletChart_DiffModelEMObs_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
+	chicklet_plot(img_name, auxm - auxo, datm, eft, lev3, cmap=plt.cm.RdBu_r)
+	img_name = 'ChicletChart_DiffModelm1Obs_'+stations[i]+'_'+Nvar+'XFtimeXTime.png'
+	chicklet_plot(img_name, auxm2 - auxo, datm, eft, lev3, cmap=plt.cm.RdBu_r)
 
