418524dc5531609f26968116bb721354cbe8c3ea
eximus <fabio_r11@hotmail.com>
2020-11-01 16:42:00 +0000

    progress on dash plots for timeseries #15

--- testTimeSeriesDash.py
+++ testTimeSeriesDash.py
@@ -2,7 +2,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import plotly.express as px
+import plotly.graph_objects as go
 
 import xarray
 
@@ -17,6 +17,8 @@ data = xarray.open_dataset(DATA_FILE)
 stations = data.stationID.data
 datetime = data.date_time.data
 
+variables = sorted([d for d in data.data_vars if len(data[d].shape) >1])
+
 # ========================
 #	  DASH APP SETUP
 # ========================
@@ -24,23 +26,41 @@ datetime = data.date_time.data
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
-	html.H2(children = DATA_FILE),
+	html.H2(children = DATA_FILE, style={'text-align':'center'}),
 
-	# TODO make another dropdown to select variables
-	# TODO make another dropdown to select ensemblers to use
-	dcc.Dropdown(
-		id='station-id',
-		options=[{'label':v, 'value':i} for i, v in enumerate(stations)],
-		# multi=True,
-		value=0
-	),
+	html.Div([
+		html.Div(children='Station:'),
+		dcc.Dropdown(
+			id='station-id',
+			options=[{'label':v, 'value':i} for i, v in enumerate(stations)],
+			value=0, placeholder='Select station...'
+		)
+	], style={'width':'33%', 'display':'inline-block'}),
+	html.Div([
+		html.Div(children='Variable: '),
+		dcc.Dropdown(
+			id='variable',
+			options=[{'label':v[v.index('_')+1:], 'value':i} for i, v in enumerate(variables[:(len(variables)//2)])],
+			value=0, placeholder='Select variable...'
+		)
+	], style={'width':'33%', 'display':'inline-block'}),
+	html.Div([
+		html.Div(children='Ensemble: '),
+		dcc.Dropdown(
+			id='ensemble',
+			options=[{'label':v, 'value':i} for i, v in enumerate(data.ensmember.data)],
+			value=[0], multi=True, placeholder='Select ensembles...'
+		)
+	], style={'width':'33%', 'display':'inline-block'}),
+
 	dcc.Graph(id='timeseries-graph'),
 
 	# TODO make a div with the slider to show the actual forecast being selected
+	html.Div(id='display-forecast-time'),
 	dcc.Slider(
 		id='forecast-time-slider',
 		min=0,
-		max=data.forecast_time.size,
+		max=data.forecast_time.size-1,
 		value=0,
 		# this creates marks only when the number of seconds represents a full day
 		# i*4 represents the index of that time so that it correctly selects this value
@@ -53,27 +73,32 @@ app.layout = html.Div([
 @app.callback(
 		Output('timeseries-graph', 'figure'),
 		[Input('station-id', 'value'),
+		Input('ensemble', 'value'),
+		Input('variable', 'value'),
 		Input('forecast-time-slider', 'value')])
-def update_graph(station, forecast_time):
-	fig = px.scatter(
-			# TODO find a way to make this work....
-			# data[
-			# dict(
-				x = datetime,
-				y = data.omega_atmp[station, :, forecast_time].data)
-				# name = 'Observed',
-				# marker = dict(color='rgb(55, 83, 109)'))])
-			# dict(
-			# 	x = datetime,
-			# 	y = data.ecmwf_atmp[station, 0, :, forecast_time].data,
-			# 	name = 'Ensemble',
-			# 	marker = dict(color='rgb(55, 83, 109)'))
-			# ])
+def update_graph(station, ensemble, variable, forecast_time):
+	fig = go.Figure()
+	fig.add_trace(go.Scatter(
+			x = datetime,
+			y = data.omega_atmp[station, :, forecast_time].data,
+			mode = 'lines', name = 'Observed'))
 
+	for e in ensemble:
+		fig.add_trace(go.Scatter(
+				x = datetime,
+				y = data.ecmwf_atmp[station, e, :, forecast_time].data,
+				mode = 'lines', name = f'Ensemble {e+1}'))
+	var_name = variables[variable][variables[variable].index('_')+1:]
 	fig.update_xaxes(title = 'Tempo')
-	fig.update_yaxes(title = 'atmp')
+	fig.update_yaxes(title = var_name)
 
 	return fig
+
+@app.callback(
+		Output('display-forecast-time', 'children'),
+		[Input('forecast-time-slider', 'value')])
+def update_forecast_time_display(forecast_time):
+	return f'Forecast time is {forecast_time} {data.forecast_time[forecast_time].data}'
 
 
 if __name__ == '__main__':
