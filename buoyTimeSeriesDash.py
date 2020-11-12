188326bb47090e6d8b240942ca51c2616438990e
eximus <fabio_r11@hotmail.com>
2020-11-12 17:17:02 +0000

    fix forecast time to match datetimes of observation, actually closes #21

--- buoyTimeSeriesDash.py
+++ buoyTimeSeriesDash.py
@@ -14,14 +14,14 @@ FONT = 'Courier New'
 # ========================
 
 # nearest
-# GFS_DATA = 'data/buoys/GFS_selectionPointNearest_20190925_20200701.nc'
-# GWES_DATA = 'data/buoys/GWES_selectionPointNearest_20190925_20200701.nc'
-# WW3_DATA = 'data/buoys/WW3_selectionPointNearest_20190925_20200701.nc'
+GFS_DATA = 'data/buoys/GFS_selectionPointNearest_20190925_20200701.nc'
+GWES_DATA = 'data/buoys/GWES_selectionPointNearest_20190925_20200701.nc'
+WW3_DATA = 'data/buoys/WW3_selectionPointNearest_20190925_20200701.nc'
 
 # average
-GFS_DATA = 'data/buoys/GFS_selectionPointAver_20190925_20200701.nc'
-GWES_DATA = 'data/buoys/GWES_selectionPointAver_20190925_20200701.nc'
-WW3_DATA = 'data/buoys/WW3_selectionPointAver_20190925_20200701.nc'
+# GFS_DATA = 'data/buoys/GFS_selectionPointAver_20190925_20200701.nc'
+# GWES_DATA = 'data/buoys/GWES_selectionPointAver_20190925_20200701.nc'
+# WW3_DATA = 'data/buoys/WW3_selectionPointAver_20190925_20200701.nc'
 
 OBSERVED_DATA = 'data/buoys/NDBC_selection_deepWaters_20190925_20200701.nc'
 
@@ -123,20 +123,24 @@ def update_graph(station, ensemble, var, forecast_time):
 		actual_time = ensmembers[list(ensmembers.keys())[0]].datetime.data
 		mask = np.in1d(obs_data.datetime, actual_time)
 		fig.add_trace(go.Scatter(
-				x = [str(t) for t in obs_data.time[mask].data],
-				y = obs_data[variables[var]][station, :][mask].data,
-				mode = 'lines+markers', name = 'Observed'))
+				x = [str(t) for t in obs_data.time.data],
+				y = obs_data[variables[var]][station, :].data,
+				mode = 'lines', name = 'Observed'))
 
 	for e in ensemble:
 		if variables[var] == 'WSPD' and 'U10m' in ensmembers[e] and 'V10m' in ensmembers[e]:
 			y = np.sqrt(ensmembers[e]['U10m'][station, :, forecast_time].data**2 + ensmembers[e]['V10m'][station, :, forecast_time].data**2)
-			fig.add_trace(go.Scatter( x = [str(t) for t in ensmembers[e].time.data], y = y, mode = 'lines', name = e))
+			fig.add_trace(go.Scatter(
+				x = [str(t) for t in (ensmembers[e].time + np.timedelta64(int(ensmembers[e][fctime_name].data[forecast_time]), 's')).data],
+				y = y, mode = 'lines', name = e))
 		elif variables[var] in ensmembers[e]:
 			if len(ensmembers[e][variables[var]].shape) == 4: # if there are multiple ensembleMembers
-				y = np.mean(ensmembers[e][variables[var]][station, :, :, forecast_time], axis=0).data
+				y = np.mean(ensmembers[e][variables[var]][station, 1:, :, forecast_time], axis=0).data
 			else:
 				y = ensmembers[e][variables[var]][station, :, forecast_time].data
-			fig.add_trace(go.Scatter( x = [str(t) for t in ensmembers[e].time.data], y = y, mode = 'lines', name = e))
+			fig.add_trace(go.Scatter(
+				x = [str(t) for t in (ensmembers[e].time + np.timedelta64(int(ensmembers[e][fctime_name].data[forecast_time]), 's')).data],
+				y = y, mode = 'lines', name = e))
 	fig.update_xaxes(title = 'Time', rangeslider_visible=True)
 	fig.update_yaxes(title = f"{variables[var]}")
 
